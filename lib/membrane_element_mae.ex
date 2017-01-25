30d262b8b09e89d19d4accd587c4cfc00fb7c90c
Filip Abramowicz <filip.abramowicz@gmail.com>
2017-01-25 23:16:51 +0100

    App main file prepared

--- lib/membrane_element_mae.ex
+++ lib/membrane_element_mae.ex
@@ -1,2 +1,14 @@
 defmodule Membrane.Element.Mae do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+
+    ]
+
+    opts = [strategy: :one_for_one, name: Membrane.Element.Mae]
+    Supervisor.start_link(children, opts)
+  end
 end

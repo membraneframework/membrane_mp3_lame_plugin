01ddc4172d73134f24a419d8676c612bf681721a
Filip Abramowicz <filip.abramowicz@gmail.com>
2017-02-10 01:03:00 +0100

    Reducing bitstring to equal number of samples added

--- lib/membrane_element_lame/encoder.ex
+++ lib/membrane_element_lame/encoder.ex
@@ -2,6 +2,7 @@ defmodule Membrane.Element.Lame.Encoder do
   use Membrane.Element.Base.Filter
   alias Membrane.Element.Lame.EncoderOptions
   alias Membrane.Element.Lame.EncoderNative
+  alias Membrane.Caps.Audio.Raw
 
 
   @doc false
@@ -20,6 +21,7 @@ defmodule Membrane.Element.Lame.Encoder do
     case EncoderNative.create(caps) do
       {:ok, native} ->
         {:ok, %{state |
+          caps: caps,
           native: native,
           queue: << >> }}
 
@@ -30,13 +32,22 @@ defmodule Membrane.Element.Lame.Encoder do
 
 
   @doc false
-  def handle_buffer({:sink, %Membrane.Buffer{payload: payload} = buffer}, %{native: native, queue: queue} = state) do
+  def handle_buffer({:sink, %Membrane.Buffer{payload: payload} = buffer}, %{native: native, queue: queue, caps: caps} = state) do
 
-    case EncoderNative.encode_buffer(native, queue <> payload) do
+    bitstring = queue <> payload
+    %Raw{format: format, channels: channels} = caps
+    {:ok, bytes_per_sample} = Raw.format_to_sample_size(format)
+    sample_size = bytes_per_sample * channels
+    nof_full_samples = byte_size(bitstring) |> div(sample_size)
+    full_sample_in_bytes = nof_full_samples * sample_size
+    <<_::size(full_sample_in_bytes)-unit(8), new_remainder::binary>> = bitstring
+
+
+    case EncoderNative.encode_buffer(native, bitstring) do
       {:error, desc} ->
         {:error, desc}
       {encoded_audio} ->
-        {:send_buffer, {encoded_audio}, state}
+        {:ok, [{:send, {:source, buffer}}], %{state | queue: new_remainder}}
     end
   end
 

ed8c13b077385cb1376644931c906ebf5eb9ae17
Mateusz Front <mateusz.front@swmansion.com>
2019-03-25 19:55:09 +0100

    maked native a private module

--- lib/membrane_element_lame/encoder_native.ex
+++ lib/membrane_element_lame/encoder_native.ex
@@ -1,7 +1,6 @@
 defmodule Membrane.Element.Lame.Encoder.Native do
-  @moduledoc """
-  This module is an interface to native lame encoder.
-  """
+  @moduledoc false
+  # This module is an interface to native lame encoder.
 
   use Unifex.Loader
 end

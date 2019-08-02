7884ed98aac4bc0f0faa8f734867507c621e9562
DominikWolek <ddww12@gmail.com>
2019-08-02 14:00:16 +0200

    Assert if result and ref files are same

--- test/integration/integration_test.exs
+++ test/integration/integration_test.exs
@@ -10,22 +10,26 @@ defmodule Lame.Encoder.IntegrationTest do
         file_src: %Element.File.Source{chunk_size: 4096, location: in_path},
         encoder: Element.Lame.Encoder,
         sink: %Element.File.Sink{location: out_path}
-      ],
-      links: %{
-        {:file_src, :output} => {:encoder, :input},
-        {:encoder, :output} => {:sink, :input}
-      }
+      ]
     })
+  end
+
+  def assert_files_equal(file_a, file_b) do
+    assert {:ok, a} = File.read(file_a)
+    assert {:ok, b} = File.read(file_b)
+    assert a == b
   end
 
   test "Encode raw samples" do
     in_path = "../fixtures/input.pcm" |> Path.expand(__DIR__)
     out_path = "/tmp/output-lame.mp3"
+    ref_path = "../fixtures/ref.mp3" |> Path.expand(__DIR__)
     on_exit(fn -> File.rm(out_path) end)
 
     assert {:ok, pid} = make_pipeline(in_path, out_path)
 
     assert Pipeline.play(pid) == :ok
     assert_end_of_stream(pid, :sink, :input, 300)
+    assert_files_equal(out_path, ref_path)
   end
 end

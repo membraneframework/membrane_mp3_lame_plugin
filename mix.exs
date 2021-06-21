95709b9797bdb27ede10dd519913f5d21b8fa0f6
Michał Śledź <michal.sledz@swmansion.com>
2021-06-21 15:59:11 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -55,14 +55,14 @@ defmodule Membrane.MP3.Lame.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.7.0"},
-      {:membrane_caps_audio_raw, "~> 0.3.0"},
+      {:membrane_caps_audio_raw, "~> 0.4.0"},
       {:membrane_caps_audio_mpeg, "~> 0.2.0"},
-      {:membrane_common_c, "~> 0.6.0"},
+      {:membrane_common_c, "~> 0.7.0"},
       {:bunch, "~> 1.0"},
       {:unifex, "~> 0.4.0"},
       {:ex_doc, "~> 0.22.2", only: :dev, runtime: false},
       {:espec, "~> 1.7", only: :test},
-      {:membrane_file_plugin, "~> 0.5.0", only: :test}
+      {:membrane_file_plugin, "~> 0.6.0", only: :test}
     ]
   end
 end

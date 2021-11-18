d23eb1495b23b15445d3393ac31e7f3518adc942
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-18 15:00:44 +0100

    bump to 0.10.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP3.Lame.Mixfile do
   use Mix.Project
 
-  @version "0.9.0"
+  @version "0.10.0"
   @github_url "https://github.com/membraneframework/membrane_mp3_lame_plugin"
 
   def project do
@@ -54,15 +54,15 @@ defmodule Membrane.MP3.Lame.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
-      {:membrane_caps_audio_raw, "~> 0.4.0"},
+      {:membrane_core, "~> 0.8.0"},
+      {:membrane_caps_audio_raw, "~> 0.5.0"},
       {:membrane_caps_audio_mpeg, "~> 0.2.0"},
-      {:membrane_common_c, "~> 0.8.0"},
+      {:membrane_common_c, "~> 0.10.0"},
       {:bunch, "~> 1.0"},
       {:unifex, "~> 0.6.0"},
       {:ex_doc, "~> 0.22.2", only: :dev, runtime: false},
       {:espec, "~> 1.7", only: :test},
-      {:membrane_file_plugin, "~> 0.6.0", only: :test}
+      {:membrane_file_plugin, "~> 0.7.0", only: :test}
     ]
   end
 end

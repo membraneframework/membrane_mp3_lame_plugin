503557e22ee1d22f72629342c314302721de0e01
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-10-03 14:11:52 +0200

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP3.Lame.Mixfile do
   use Mix.Project
 
-  @version "0.16.0"
+  @version "0.16.1"
   @github_url "https://github.com/membraneframework/membrane_mp3_lame_plugin"
 
   def project do
@@ -70,16 +70,16 @@ defmodule Membrane.MP3.Lame.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.12.0"},
-      {:membrane_raw_audio_format, "~> 0.10.0"},
+      {:membrane_raw_audio_format, "~> 0.11.0"},
       {:membrane_caps_audio_mpeg, "~> 0.2.0"},
       {:membrane_common_c, "~> 0.15.0"},
       {:bunch, "~> 1.0"},
       {:unifex, "~> 1.0"},
-      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:espec, "~> 1.7", only: [:dev, :test]},
-      {:membrane_file_plugin, "~> 0.13.0", only: :test},
-      {:credo, "~> 1.6", runtime: false, only: :dev},
-      {:dialyxir, ">= 0.0.0", runtime: false, only: :dev}
+      {:membrane_file_plugin, "~> 0.15.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end

a69eaa6c095e9a25f1d892326ef25b446c508aac
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2024-03-26 11:18:56 +0100

    Update test/integration/integration_test.exs
    Co-authored-by: Mateusz Front <mateusz.front@swmansion.com>

--- test/integration/integration_test.exs
+++ test/integration/integration_test.exs
@@ -102,8 +102,6 @@ defmodule Membrane.MP3.Lame.Encoder.IntegrationTest do
           |> child(:sink_encoded, Membrane.Testing.Sink)
       )
 
-    assert_end_of_stream(raw_pipeline, :sink_raw, :input, 500)
-    assert_end_of_stream(encoded_pipeline, :sink_encoded, :input, 500)
 
     assert_correct_sink_buffers_pts(raw_pipeline, encoded_pipeline, raw_to_encoded_buffer_ratio)
   end

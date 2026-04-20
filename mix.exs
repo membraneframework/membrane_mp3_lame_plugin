70379b87bcb362e519179d39d2131e532d95a5d7
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-20 14:06:03 +0200

    Update mix.lock

--- mix.exs
+++ mix.exs
@@ -55,11 +55,13 @@ defmodule Membrane.MP3.Lame.Mixfile do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling]
+      flags: [:error_handling],
+      plt_add_apps: [:syntax_tools]
     ]
 
     if System.get_env("CI") == "true" do
       # Store PLTs in cacheable directory for CI
+      File.mkdir_p!(Path.join([__DIR__, "priv", "plts"]))
       [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
     else
       opts
@@ -77,9 +79,9 @@ defmodule Membrane.MP3.Lame.Mixfile do
       {:membrane_precompiled_dependency_provider, "~> 0.2.1"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
       {:membrane_raw_audio_parser_plugin, "~> 0.4.0", only: :test},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:credo, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false}
     ]
   end
 end
